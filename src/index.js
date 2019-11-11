6a53142fa2efa196f650486a574a76eea3b38c80
liwei <778746568@qq.com>
2019-11-11 18:17:50 +0800

    项目初始化保存

---
+++ src/index.js
@@ -0,0 +1,6 @@
+"use strict";
+var a = 123;
+var h1 = document.createElement("h1");
+h1.innerHTML = "hello TypeScript!";
+document.body.appendChild(h1);
+console.log(a);
---
+++ src/index.ts
@@ -0,0 +1,5 @@
+let a:number = 123;
+const h1 = document.createElement("h1");
+h1.innerHTML = "hello TypeScript!";
+document.body.appendChild(h1);
+console.log(a);
